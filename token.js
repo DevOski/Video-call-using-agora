4bd8f13a04bf1568a0e875ebc150e5ef27ab1d74
DevOski <113361825+DevOski@users.noreply.github.com>
2022-11-21 16:23:45 +0500

    Create token.js

---
+++ token.js
@@ -0,0 +1,10 @@
+App id
+270b512970864b0a93b14650e52e8f9c
+
+channel name
+Testing
+
+token
+007eJxTYEiYtOmzmUH3skXHyvtq+3XPv0mLqbm07PvBuzFtv7NFr/goMBiZGySZGhpZmhtYmJkkGSRaGicZmpiZGqSaGqVapFkmu0dUJzcEMjKs2BHCwsgAgSA+O0NIanFJZl46AwMAXDIiWw==
+
+  Token expires on November 22, 2022 10:51 AM UTC
